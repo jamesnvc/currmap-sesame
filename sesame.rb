43d3fb93e4cb2e260078ec391cbae8b17a918c30
James N. V. Cash <james.nvc@gmail.com>
2010-05-12 16:56:46 -0400

    More work for Sesame ORM, fixing to work with Sesame 2.x

--- sesame.rb
+++ sesame.rb
@@ -1,19 +1,73 @@
 require 'net/http'
+require 'uri'
+require 'rubygems'
+require 'nokogiri'
 
 module Sesame
 
   class Server
-    def initialize(host, port, sesamedir,options=nil)
+    def initialize(host, port, sesamedir, repo, user, pass, options=nil)
       @host = host
       @port = port
       @sesamedir = sesamedir
+      @repo = repo
+      @user = user
+      @pass = pass
       @options = options
-      @queryuri = "http://#{@host}:#{@port}/#{@sesamedir}/servlets/evaluateTableQuery"
+      @uri =
+        URI.parse("http://#{@host}:#{@port}/#{@sesamedir}/repositories")
     end
 
-    def query(query_str)
-      req = Net::HTTP::Post.new(@queryuri)
-      req["content-type"] = "multipart/form-data"
+    def select(query_str, query_lang="sparql")
+      params = { "query" => query_str, "queryLn" => query_lang }
+      query_params = params.collect { |k,v| "#{k}=#{URI.escape(v.to_s)}" }.
+        reverse.join '&'
+      req = Net::HTTP::Get.new("#{@uri.path}/#{@repo}?#{query_params}",
+                               {'Accept' => 'application/rdf+xml' })
+      req.basic_auth @user, @pass
+      return request(req).body
+    end
+    
+    def list_repos
+      req = Net::HTTP::Get.new(@uri.path,
+                               {'Accept' => "application/sparql-results+xml" })
+      xml = Nokogiri::XML.parse(request(req).body)
+      res = Array.new
+      xml.xpath('//xmlns:binding[@name="id"]/xmlns:literal',
+                xml.root.namespaces ).each do |repo|
+        res << repo.content
+      end
+      return res
+    end
+
+    def list_namespaces repo
+      req = Net::HTTP::Get.new("#{@uri.path}/#{repo}/namespaces",
+                               {'Accept' => "application/sparql-results+xml" })
+      xml =  Nokogiri::XML.parse(request(req).body)
+      res = Hash.new
+      xml.xpath('//xmlns:result').each do |result|
+        prefix = result.xpath('//xmlns:binding[@name="prefix"]/xmlns:literal')[0].content
+        namespc = result.xpath('//xmlns:binding[@name="namespace"]/xmlns:literal')[0].content
+        res[prefix] = namespc
+      end
+      return res
+    end
+    
+    def request(req)
+      res = Net::HTTP.start(@host, @port) {|http|
+        http.request(req)
+      }
+      if (not res.kind_of?(Net::HTTPSuccess))
+        handle_error(req, res)
+      end
+      res
+    end
+
+    private
+
+    def handle_error(req, res)
+      e = RuntimeError.new("#{res.code}:#{res.message}\nMETHOD:#{req.method}\nURI:#{req.path}\n#{res.body}")
+      raise e
     end
   end
 end
