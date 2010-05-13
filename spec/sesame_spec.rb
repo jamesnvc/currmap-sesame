930e1d4c6cdd1d37728d2e3648efe64995471f1c
James N. V. Cash <james.nvc@gmail.com>
2010-05-13 14:03:38 -0400

    Moving other tests over to RSpec

--- spec/sesame_spec.rb
+++ spec/sesame_spec.rb
@@ -1,29 +1,6 @@
-require File.expand_path(File.dirname(__FILE) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'rubygems'
 require 'parseconfig'
-
-
-def select_serql_test
-  serv = new_test_serv
-  query = <<EOF
-SELECT C
-FROM {C} rdf:type {base:Course}
-USING NAMESPACE base = <file://schema.xml#>
-EOF
-  puts serv.select(query, "serql")
-end
-
-def select_sparql_test
-  serv = new_test_serv
-  query = <<EOF
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX base: <file://schema.xml>
-SELECT ?x
-WHERE
-{ ?x rdf:type base:Course }
-EOF
-  puts serv.select(quer, "sparql")
-end
 
 describe Sesame::Server do
 
@@ -41,18 +18,39 @@ describe Sesame::Server do
     @serv = Sesame::Server.new(@host, @port, @sesame_dir, @repo, @user, @pass)
   end
   
-  it "gets a list of repos" do
+  it "should get a list of repos" do
     repos = @serv.list_repos
     repos.should == [ "SYSTEM", "CurrMap" ]
   end
 
-  it "gets a list of all namespaces" do namespaces = @serv.list_namespaces
-    "CurrMap" namespaces.should == {
+  it "should get a list of all namespaces" do
+    namespaces = @serv.list_namespaces "CurrMap"
+    namespaces.should == {
       "rdfs"=>"http://www.w3.org/2000/01/rdf-schema#",
       "xsd"=>"http://www.w3.org/2001/XMLSchema#",
       "rdf"=>"http://www.w3.org/1999/02/22-rdf-syntax-ns#",
       "base"=>"file://schema.xml#"
     }
+  end
+
+  it "should run a SeRQL query" do
+    query = <<EOF
+SELECT C
+FROM {C} rdf:type {base:Course}
+USING NAMESPACE base = <file://schema.xml#>
+EOF
+    res = @serv.select(query, "serql")
+  end
+
+  it "should run a SPARQL query" do
+    query = <<EOF
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX base: <file://schema.xml>
+SELECT ?x
+WHERE
+{ ?x rdf:type base:Course }
+EOF
+    res =  @serv.select(query, "sparql")
   end
 
 end
