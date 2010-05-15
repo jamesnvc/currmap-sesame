b7cae066ef76b967c5ad6ef4e9d557ad50f40fcf
James N. V. Cash <james.nvc@gmail.com>
2010-05-15 13:20:50 -0400

    Refactoring common code for XML processing methods

--- lib/sesame.rb
+++ lib/sesame.rb
@@ -8,60 +8,69 @@ module Sesame
   class Server
     attr_reader :uri, :repo
     
-    def initialize(host, port, sesamedir, repo, user, pass, options=nil)
+    def initialize(host, port, sesamedir, repo, user, pass, options={})
       @host = host
       @port = port
       @sesamedir = sesamedir
       @repo = repo
       @user = user
       @pass = pass
-      @options = options
+      @opt = Hash.new(false).merge options
       @uri =
         URI.parse("http://#{@host}:#{@port}/#{@sesamedir}/repositories")
+    end
+    
+    def process_query_xml(uri, &block)
+      req = Net::HTTP::Get.new(@uri.path + uri, { 'Accept' => 'application/sparql-results+xml'})
+      req.basic_auth @user, @pass
+      xml = Nokogiri::XML.parse(request(req).body)
+      if !@opt[:return_xml]
+        res = yield xml
+        return res
+      else
+        return xml
+      end
     end
 
     def select(query_str, query_lang="sparql")
       params = { "query" => query_str, "queryLn" => query_lang }
-      query_params = params.collect { |k,v| "#{k}=#{URI.escape(v.to_s)}" }.
-        reverse.join '&'
-      req = Net::HTTP::Get.new("#{@uri.path}/#{@repo}?#{query_params}",
-                               {'Accept' => 'application/sparql-results+xml' })
-      req.basic_auth @user, @pass
-      xml = Nokogiri::XML.parse(request(req).body)
-      res = Hash.new
-      # Get variables from query
-      xml.xpath('//xmlns:head/xmlns:variable').each do |var|
-        res[var['name']] = Array.new
+      query_params =
+        params.collect { |k,v| "#{k}=#{URI.escape(v.to_s)}" }.reverse.join '&'
+      
+      process_query_xml "/#{@repo}?#{query_params}" do |xml|
+        res = Hash.new
+        # Get variables from query
+        xml.xpath('//xmlns:head/xmlns:variable').each do |var|
+          res[var['name']] = Array.new
+        end
+        # Populate result hash by variable
+        xml.xpath('//xmlns:result/xmlns:binding').each do |binding|
+          res[binding['name']] << (binding / 'uri')[0].content
+        end
+        res # res should be the result of the block
       end
-      # Populate result hash by variable
-      xml.xpath('//xmlns:result/xmlns:binding').each do |binding|
-        res[binding['name']] << (binding / 'uri')[0].content
-      end
-      return res
     end
     
     def list_repos
-      req = Net::HTTP::Get.new(@uri.path,
-                               {'Accept' => "application/sparql-results+xml" })
-      xml = Nokogiri::XML.parse(request(req).body)
-      res = Array.new
-      xml.xpath('//xmlns:binding[@name="id"]/xmlns:literal').each do |repo|
-        res << repo.content
+      process_query_xml "" do |xml|
+        res = Array.new
+        xml.xpath('//xmlns:binding[@name="id"]/xmlns:literal').each do |repo|
+          res << repo.content
+        end
+        res # res should be the result of the block
       end
-      return res
     end
 
     def list_namespaces repo
-      req = Net::HTTP::Get.new("#{@uri.path}/#{repo}/namespaces",
-                               {'Accept' => "application/sparql-results+xml" })
-      xml =  Nokogiri::XML.parse(request(req).body)
-      res = Hash.new
-      xml.xpath('//xmlns:result').each do |result|
-        prefix = result.xpath('.//xmlns:binding[@name="prefix"]/xmlns:literal')[0].content
-        namespc = result.xpath('.//xmlns:binding[@name="namespace"]/xmlns:literal')[0].content
-        res[prefix] = namespc
+      process_query_xml "/#{repo}/namespaces" do |xml|
+        res = Hash.new
+        xml.xpath('//xmlns:result').each do |result|
+          prefix = result.xpath('.//xmlns:binding[@name="prefix"]/xmlns:literal')[0].content
+          namespc = result.xpath('.//xmlns:binding[@name="namespace"]/xmlns:literal')[0].content
+          res[prefix] = namespc
+        end
+        res # res should be the result of the block
       end
-      return res
     end
     
     def request(req)
